670f19167734e70982f5bec9356bffd87fd683eb
MayankVagadiya <51834601+mayankvagadia@users.noreply.github.com>
2023-05-20 17:27:30 +0530

    initial commit

---
+++ app/src/main/java/com/ListedCo/listedcotask/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.ListedCo.listedcotask.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ListedCo.listedcotask.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
